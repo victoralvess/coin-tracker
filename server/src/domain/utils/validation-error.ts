603a9f8d10a31745fa7b1f420145aae25fe53c66
Victor Alves <victoralves28.pg@gmail.com>
2024-05-24 18:46:21 -0300

    chore: run lint and fix errors

--- server/src/domain/utils/validation-error.ts
+++ server/src/domain/utils/validation-error.ts
@@ -1,6 +1,6 @@
 export class ValidationError extends Error {
     public readonly status = 400;
-
+    
     constructor(message: string, public readonly errors: string[]) {
         super(message);
         this.name = ValidationError.name;

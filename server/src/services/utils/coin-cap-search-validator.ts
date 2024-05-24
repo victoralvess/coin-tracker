597d86fe3124765474855d637f8392b0dcdcaaba
Victor Alves <victoralves28.pg@gmail.com>
2024-05-24 17:55:42 -0300

    feat: implement search resullts validator

---
+++ server/src/services/utils/coin-cap-search-validator.ts
@@ -0,0 +1,37 @@
+import { CoinCapSearch } from '../helpers/coin-cap-search';
+import { CryptoCoin } from '../../domain/models/crypto-coin';
+import { Validator } from '../../domain/utils/validator';
+import Joi, { type Schema } from 'joi';
+
+export class CoinCapSearchValidator implements Validator<CoinCapSearch, CryptoCoin[]> {
+    private schema: Schema<CoinCapSearch>;
+
+    constructor() {
+        this.schema = Joi.object({
+            data: Joi.array().items(
+                Joi.object({
+                    id: Joi.string(),
+                    priceUsd: Joi.number().unsafe().precision(2).cast('string'),
+                    symbol: Joi.string()
+                })
+            )
+        });
+    }
+
+    validate(input: CoinCapSearch): CryptoCoin[] {
+        const value = this.schema.validate(input, { allowUnknown: true });
+
+        if (value.error !== undefined) {
+            throw Error('Unexpected server response');
+        }
+
+        return value.value.data.map((coin) => {
+            return {
+                id: coin.id,
+                price: Number(coin.priceUsd),
+                icon: '', // TBD
+                symbol: coin.symbol
+            };
+        })
+    }
+}

f2f8d956c294ff5a12a2b4c814ea5c527172c924
Victor Alves <victoralves28.pg@gmail.com>
2024-05-24 19:03:20 -0300

    feat: add CoinCapCoin type to describe a single coin

--- server/src/services/helpers/coin-cap-search.ts
+++ server/src/services/helpers/coin-cap-search.ts
@@ -1,15 +1,5 @@
+import { CoinCapCoin } from './coin-cap-coin';
+
 export type CoinCapSearch = {
-    data: {
-        id: string;
-        rank: string;
-        symbol: string;
-        name: string;
-        supply: string;
-        maxSupply: string;
-        marketCapUsd: string;
-        volumeUsd24Hr: string;
-        priceUsd: string;
-        changePercent24Hr: string;
-        vwap24Hr: string;
-    }[];
+    data: CoinCapCoin[];
 };

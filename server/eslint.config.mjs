06278492e35e57bf3a4c17693e2486b431ae9c76
Victor Alves <victoralves28.pg@gmail.com>
2024-05-24 15:48:38 -0300

    chore: set up eslint

---
+++ server/eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'semi': 'off',
+      '@typescript-eslint/semi': 'error',
+      '@typescript-eslint/member-delimiter-style': 'error'
+    }
+  }
+);

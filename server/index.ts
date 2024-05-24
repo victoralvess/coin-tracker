b4b42cfb84d4b0192d034a3f0c943585a532d4a9
Victor Alves <victoralves28.pg@gmail.com>
2024-05-24 15:49:33 -0300

    chore: run lint and fix errors

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,14 @@
-import { env } from 'node:process'
-import express from 'express'
-import type { Request, Response } from 'express'
+import { env } from 'node:process';
+import express from 'express';
+import type { Request, Response } from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/ping', (req: Request, res: Response) => {
-    res.send('PONG')
-})
+    res.send('PONG');
+});
 
-const PORT = Number(env.PORT ?? 3000)
+const PORT = Number(env.PORT ?? 3000);
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
-})
+    console.log(`Server running at http://localhost:${PORT}`);
+});

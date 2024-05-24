7df92f6d720f276381c985bef0d7a6468a79ebf4
Victor Alves <victoralves28.pg@gmail.com>
2024-05-24 18:19:38 -0300

    feat: add use case to let users search for coins

--- server/src/application/use-cases/search-coins.ts
+++ server/src/application/use-cases/search-coins.ts
@@ -1,10 +1,15 @@
 import type { CryptoCoin } from '../../domain/models/crypto-coin';
+import { Validator } from '../../domain/utils/validator';
 import type { CoinCapService } from '../../services/coin-cap-service';
 
 export class SearchCoins {
-    constructor(private readonly coinCapService: CoinCapService) { }
+    constructor(
+        private readonly coinCapService: CoinCapService,
+        private readonly paramsValidator: Validator<{ q: string }, { q: string }>
+    ) { }
 
     async searchCoins(params: { q: string }): Promise<{ data: CryptoCoin[] }> {
+        params = this.paramsValidator.validate(params);
         const result = await this.coinCapService.search(params.q);
         return { data: result };
     }

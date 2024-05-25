3d518f8ab5fbd6135704a4561f8419d902f6dcb3
Victor Alves <victoralves28.pg@gmail.com>
2024-05-25 11:32:24 -0300

    feat: add main page to load cryptos

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,79 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import CryptoCard from './components/CryptoCard.vue'
+import { Crypto } from './types/crypto';
+import axios from 'axios';
+
+const localStorageKey = 'crypto';
+
+const checkLocalStorage = () => {
+  if (localStorage.getItem(localStorageKey)) {
+    savedCrypto.value = JSON.parse(localStorage.getItem(localStorageKey)!);
+  }
+}
+
+const savedCrypto = ref<Crypto[]>([])
+checkLocalStorage();
+
+const addCrypto = (crypto: Crypto): void => {
+  savedCrypto.value = [...savedCrypto.value, crypto];
+  updateLocalStorage();
+}
+
+const removeCrypto = (crypto: Crypto): void => {
+  savedCrypto.value = savedCrypto.value.filter(c => c.id !== crypto.id);
+  updateLocalStorage();
+}
+
+const updateLocalStorage = () => {
+  localStorage.setItem(localStorageKey, JSON.stringify(savedCrypto.value));
+}
+
+const isSaved = (crypto: Crypto): boolean => {
+  return savedCrypto.value.findIndex(c => c.id === crypto.id) !== -1;
+}
+
+
+const searchText = ref<string>('');
+const searchResults = ref<Crypto[]>([]);
+const searchTimeout = ref<number | undefined>();
+
+const searchCrypto = (): void => {
+  clearTimeout(searchTimeout.value);
+  searchTimeout.value = setTimeout(async () => {
+    try {
+      const result = await axios
+        .get<{ data: Crypto[] }>(
+          'http://localhost:3000/coins',
+          { params: { q: searchText.value } }
+        );
+
+      searchResults.value = result.data.data;
+    } catch {
+      searchResults.value = [];
+    }
+  }, 300);
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="container">
+    <div>
+      <div class="flex flex-col flex-1 gap-4 items-center">
+        <div class="my-8 max-w-screen-md w-full">
+          <input type="search" v-model="searchText" @input="searchCrypto" placeholder="Search for a cryptocurrency"
+            class="px-4 py-2 w-full border-b rounded-t-md focus:outline-none bg-slate-600 caret-slate-50 text-white">
+        </div>
+        <div class="max-w-screen-md w-full">
+          <div class="flex flex-col md:flex-row gap-4 overflow-x-auto">
+            <div v-for="crypto in searchResults" :key="crypto.id">
+              <CryptoCard :crypto="crypto" :saved="isSaved(crypto)" @add="addCrypto" @remove="removeCrypto" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>

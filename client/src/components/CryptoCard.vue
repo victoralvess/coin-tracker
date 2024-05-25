b20049581743d93b170c13005db72698a4898fb0
Victor Alves <victoralves28.pg@gmail.com>
2024-05-25 13:05:48 -0300

    feat: add realtime price tracking

--- client/src/components/CryptoCard.vue
+++ client/src/components/CryptoCard.vue
@@ -1,40 +1,123 @@
 <script setup lang="ts">
-import { Crypto } from '../types/crypto';
+import { ref } from 'vue';
 
-defineProps<{ crypto: Crypto, saved: boolean }>()
-defineEmits<{
-  add: [crypto: Crypto],
-  remove: [crypto: Crypto],
-  preview: [crypto: Crypto]
-}>()
+import { Crypto } from '../types/crypto';
+import Modal from './Modal.vue';
+import { ApexOptions } from 'apexcharts';
+
+const props = defineProps<{ crypto: Crypto, saved: boolean }>()
+defineEmits<{ add: [crypto: Crypto], remove: [crypto: Crypto] }>()
 
 const setDefaultIcon = (evt: Event): void => {
   (evt.target! as HTMLImageElement).src
     = 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/white/generic.png'
 }
+
+const modalOpen = ref<boolean>(false);
+const toggleModal = () => {
+  modalOpen.value = !modalOpen.value;
+  toggleWs();
+};
+
+const chartSeries = ref<{ name: string, data: [number, number][] }>({
+  name: props.crypto.symbol,
+  data: []
+});
+
+const pricesWs = ref<WebSocket | null>(null);
+const toggleWs = () => {
+  if (modalOpen.value) {
+    pricesWs.value = new WebSocket(`wss://ws.coincap.io/prices?assets=${props.crypto.id}`);
+    pricesWs.value.onmessage = (msg: { data: string }): void => {
+      try {
+        chartSeries.value.data
+          .push([Date.now(), Number(JSON.parse(msg.data)[props.crypto.id])]);
+      } catch { }
+    }
+  } else if (pricesWs.value !== null) {
+    pricesWs.value.close();
+    pricesWs.value = null;
+  }
+}
+
+const chartOptions: ApexOptions = {
+  chart: {
+    type: 'area',
+    stacked: false,
+    height: 350,
+    zoom: {
+      enabled: false,
+      autoScaleYaxis: true
+    },
+    toolbar: {
+      show: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  markers: {
+    size: 0
+  },
+  title: {
+    text: `${props.crypto.symbol} Price Movement`,
+    align: 'center'
+  },
+  colors: ['#6366f1'],
+  fill: {
+    type: 'gradient',
+    colors: ['#6366f1'],
+    gradient: {
+      shadeIntensity: 1,
+      inverseColors: false,
+      opacityFrom: 0.5,
+      opacityTo: 0,
+      stops: [0, 90, 100]
+    }
+  },
+  yaxis: {
+    title: {
+      text: 'Price'
+    }
+  },
+  xaxis: {
+    type: 'datetime',
+    labels: {
+      datetimeUTC: false
+    }
+  }
+};
 </script>
 
 <template>
-  <div
-    class="relative flex gap-5 items-center rounded-lg border min-w-[280px] w-full h-[100px] px-4 bg-slate-600 text-white">
-    <div>
-      <img :src="crypto.icon" class="w-auto h-[60px]" @error="setDefaultIcon">
+  <div>
+    <div
+      class="relative flex gap-5 items-center rounded-lg border min-w-[280px] w-full h-[100px] px-4 bg-slate-600 text-white">
+      <div>
+        <img :src="crypto.icon" class="w-auto h-[60px]" @error="setDefaultIcon">
+      </div>
+      <div>
+        <h3 class="font-bold mt-2">{{ crypto.symbol }}</h3>
+        <p class="text-sm mt-1">${{ crypto.price.toFixed(2) }}</p>
+      </div>
+      <div class="absolute top-4 right-4">
+        <button v-if="saved" @click="toggleModal" class="mr-3">
+          <i class="fa-solid fa-magnifying-glass-chart text-xl text-white-400 hover:text-gray-200"></i>
+        </button>
+        <button v-if="saved" @click="$emit('remove', crypto)">
+          <i class="fa-solid fa-circle-minus text-xl text-white-400 hover:text-gray-200"></i>
+        </button>
+        <button v-else @click="$emit('add', crypto)">
+          <i class="fa-solid fa-circle-plus text-xl text-white-400 hover:text-gray-200"></i>
+        </button>
+      </div>
     </div>
-    <div>
-      <h3 class="font-bold mt-2">{{ crypto.symbol }}</h3>
-      <p class="text-sm mt-1">${{ crypto.price.toFixed(2) }}</p>
-    </div>
-    <div class="absolute top-4 right-4">
-      <button v-if="saved" @click="$emit('preview', crypto)" class="mr-3">
-        <i class="fa-solid fa-magnifying-glass-chart text-xl text-white-400 hover:text-gray-200"></i>
-      </button>
-      <button v-if="saved" @click="$emit('remove', crypto)">
-        <i class="fa-solid fa-circle-minus text-xl text-white-400 hover:text-gray-200"></i>
-      </button>
-      <button v-else @click="$emit('add', crypto)">
-        <i class="fa-solid fa-circle-plus text-xl text-white-400 hover:text-gray-200"></i>
-      </button>
-    </div>
+
+    <Modal :modal-open="modalOpen" @close-modal="toggleModal">
+      <div>
+        <apexchart width="500" type="area" :options="chartOptions" :series="[chartSeries]"></apexchart>
+      </div>
+    </Modal>
   </div>
 </template>
 
